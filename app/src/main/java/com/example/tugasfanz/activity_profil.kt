6da29532a020d5cd2f2e825602a5e0dd8649398c
fauzan1701 <fznazani@gmail.com>
2023-07-13 09:49:28 +0700

    project  baru

---
+++ app/src/main/java/com/example/tugasfanz/activity_profil.kt
@@ -0,0 +1,11 @@
+package com.example.tugasfanz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_profil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
